dcfee09e4c4e730f3e0f3170256eedac80dd8665
playvora <longliator.dev@gmail.com>
2026-07-01 18:21:49 +0700

    fux

--- WindowsServer/main.c
+++ WindowsServer/main.c
@@ -1,3 +1,4 @@
+#include <initguid.h>
 #define WIN32_LEAN_AND_MEAN
 #include <winsock2.h>
 #include <ws2tcpip.h>
@@ -13,7 +14,6 @@
 #pragma comment(lib, "ws2_32.lib")
 #pragma comment(lib, "ole32.lib")
 #pragma comment(lib, "avrt.lib")
-#pragma comment(lib, "uuid.lib")
 
 #define FrameSamples 960
 #define MaxClients 64
